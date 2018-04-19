8ab1c9d77ea01a39eb80f5c41bbd17415dda3cb2
ClaudiaInBytes <claudiainbytes@gmail.com>
2018-04-19 15:27:30 -0500

    Setting Yarn, Webpack and Babel

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+/*
+    ./webpack.config.js
+*/
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './public/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  mode: 'production',  
+  target: 'node',
+  entry: ["./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: path.resolve(__dirname, "public"),
+    filename: "js/index_bundle.js"
+  },
+  module: {
+    rules: [
+      { test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }, {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }, {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "babel-loader"
+          },
+          {
+            loader: "react-svg-loader",
+            options: {
+              jsx: true // true outputs JSX tags
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
